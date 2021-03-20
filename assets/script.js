9bc6671783970d9229a89d0ed268ac6c76e9dfec
Old Prego <spymasterluke@gmail.com>
2021-03-20 12:26:07 -0500

    Finished functionality of template literal and appended it to the page

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,8 @@
-// This section will be for appending cards onto the main page
-
-// var myHTMLTemplate = (petName,petImg,petBreed,petAge,petOwners,petDist,petLoc) =>
-//     <article>
-//         <h4>${petName}</h4>
-//         <img
-//     </article>
-var petLocation = {
-    shelter: "Luke's Shelter",
-    address1: "315 6th Ave E",
-    address2: "Apt 6",
-    city: "Alexandria",
-    state: "MN",
-    zip: "56308"
-};
-
-
 // Template tag parser courtesy of Nate
 // This takes template tags and parses them into HTML elements that are appendable to the current scope
-helperconst html = (strings, ...values) => new DOMParser().parseFromString(strings.map((string, i) => strings[i] + values[i]).join(''), "text/html").body.firstChild;
+const html = (strings, ...values) => new DOMParser().parseFromString(strings.map((string, i) => strings[i] + values[i]).join(''), "text/html").body.firstChild;
+
+var petList = document.getElementById("petList");
 
 function buildPetCard(petName,petImg,petBreed,petAge,petOwners,petDist,petLoc){
     
@@ -40,4 +25,25 @@ function buildPetCard(petName,petImg,petBreed,petAge,petOwners,petDist,petLoc){
                 <p>${petLoc.zip}</p>
             </div>
         </div>`
-}
+
+    return petCard;
+};
+
+var petLocation = {
+    shelter: "Luke's Shelter",
+    address1: "315 6th Ave E",
+    address2: "Apt 6",
+    city: "Alexandria",
+    state: "MN",
+    zip: "56308"
+};
+
+var petName = "Sanders";
+var petImg = "http://placehold.it/450x500";
+var petBreed = "Tabby";
+var petAge = "4";
+var petOwners = "1";
+var petDist = "20";
+
+var petCard = buildPetCard(petName,petImg,petBreed,petAge,petOwners,petDist,petLocation);
+petList.appendChild(petCard);
