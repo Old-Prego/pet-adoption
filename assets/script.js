89128b3ebbeb26d6de6947add5a9c394ce2d940f
Abenezer Merdassa <abenezermerdassa@gmail.com>
2021-03-22 09:10:32 -0500

    Two possible ways to interact with the API

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,28 @@
-const url = "https://api.petfinder.com/v2/types/dog";
-fetch(url, {
+fetch('https://api.petfinder.com/v2/animals', {
+  method : 'GET' ,
+  withCredentials: true,
+  mode: 'no-cors',
   headers: {
-    "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJqNHNDWnV2d3BmZ0JKQkprY1RGMVEyaldLM2ltVDJndHNkT1VpQzNRd0tqdExhaHNZUCIsImp0aSI6ImU1MTE5OGQzMDM2ODU4ZDA5M2ZmODNlMDkwZjNlZmM3YTIwNmU4Zjk5ZjZlOTg0ODk5M2RjMGM5MDQwNTY4ZjExZGUxNTdmNmFlNTk4MDJkIiwiaWF0IjoxNjE2MjYxNTQxLCJuYmYiOjE2MTYyNjE1NDEsImV4cCI6MTYxNjI2NTE0MSwic3ViIjoiIiwic2NvcGVzIjpbXX0.yCmthYyGDPHz-4SZHRPjYKw-vjumv7iN9xGSSDnTorMOonAuvXp6-DsUX8bhroXGU1xw9hOyACjEZqWw-s0kaliAkpXb0zb0GWHVrFEKHyy9Ft_EDf4Nxjq0crEgxguuEZdcfMvc59fmO99CpzMiPjUdDe6lTMdEhk2DhGaVK6fK4jFA26UGXSSwWjIDbjHkgGvrMp5VCe-tGAtwG-d60qHFxU-b5T4DL1hs2wWwjB6OYKevDb-S8HoIGLucdXmS-5JLN8-AURYE7OmWRfoz2vMF6A8Jh4iA9Bf5vM3J1XaFwSTQXADXB7hia3B1uuBaDkIxW1q9c1mpGQTfQB1deg"
-
+    'Authorization' : 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJqNHNDWnV2d3BmZ0JKQkprY1RGMVEyaldLM2ltVDJndHNkT1VpQzNRd0tqdExhaHNZUCIsImp0aSI6IjQ5MWI5NmQ2ZjFmZDI5NDU1YTg5ZjJiNWVjMGFiNTBkNDIxMDRkZmQzMGFjMmQ5NjQwN2RhMmU0NzA4MDdiZTNlN2NkYTg0YzY0NDc5MmNmIiwiaWF0IjoxNjE2MzkxNTM4LCJuYmYiOjE2MTYzOTE1MzgsImV4cCI6MTYxNjM5NTEzOCwic3ViIjoiIiwic2NvcGVzIjpbXX0.CZUGhs6INYQ94ACjSC12bAoD_c5Wn7AKyrqlGLgkhyoFlXiL_p044IeURrVK242vz25WJ2FDDXdEN6c5iBL_4xpGZSLDnn6AYW23myYWRwRXC53Lf-UKu2aP6vbebh-lJ6GjPIM2Ut4KU9Yu8svzLpass--EthWDfHAJmCYVZRisE9YEDx9F07RW5HcV_wzW58XRbfei4XdCyU1tGwxiPSP2rE-86ubr8FRi2cEu4qnOpBQYa_0T0EXBa3OlnhjypyCkR2d84qtjHB0ZAoTi-Kn62bt_Y4ENq1oiumS1ZDQGcdVHE0vFeebRKp2VK0CpY1B-eckncMFBz21mt1EYzw'
   }
+}
+).then(res =>{
+ console.log("RESPONSE", res)
+}).catch(e =>{
+  console.log("OH! NO", e)
 })
-.then(resp => resp.json())
-  .then(function(data) {
-    console.log(data);
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
+//Does not work, I got past one thing but i am having some real issues with authentication,
+
+
+//Another method I found of getting data was a SDK for JS but even that seems like a dead-end looking for more options...
+var petfinder = require("@petfinder/petfinder-js");
+var pf = new petfinder.Client({apiKey: " j4sCZuvwpfgBJBJkcTF1Q2jWK3imT2gtsdOUiC3QwKjtLahsYP", secret: "aN0ZxQr0R1rBU7ikZCowpLOuVUQDqE0Z65Ck6Glb"});
+
+pf.animal.search()
+    .then(function (response) {
+        console.log('data...')
+        return response.json()
+    })
+    .catch(function (error) {
+        console.log('oh noooo', error)
+    });
