1b0bdd4d07278f48d60a8dbc2fe0fdcec793f5cd
DeerRan <77216956+DeerRan@users.noreply.github.com>
2021-03-25 00:07:50 -0500

    general updates that correspond with HTML updates (i.e. style for new elements)

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,35 @@
-
 // Template tag parser courtesy of Nate
 // This takes template tags and parses them into HTML elements that are appendable to the current scope
 const html = (strings, ...values) => new DOMParser().parseFromString(strings.map((string, i) => strings[i] + values[i]).join(''), "text/html").body.firstChild;
 
 var petList = document.getElementById("petList");
 
-function buildPetCard(petName,petImg,petBreed,petAge,petOwners,petDist,petLoc){
-    
-    
-    let petCard = html`
+function buildPetCard(petName, petImg, petBreed, petAge, petOwners, petDist, petLoc, petPrice) {
 
-        <div id="${petName}" class="petCard card cell medium-6 large-4">
 
-            <h3 class="card-divider">${petName}</h3>
-
-            <div class="card-section">
-                <img src="${petImg}" alt="Picture of ${petName}" width="200px" height="200px">
+    let petCard = html `
+    <div class='columns small-12 medium-4 large-3 end'>
+    <div class='card-container'>
+        <div id='${petName}' class="card-flex-animal card">
+            <div class="card-image">
+                <img src="${petImg}">
+                <span id='nameofpet' class="label card-name">${petName}</span>
             </div>
-
             <div class="card-section">
-
-                <p>Breed: ${petBreed}</p>
-                <p>Age: ${petAge}</p>
-                <p>Previous Owners: ${petOwners}</p>
-                <p>Distance Away: ${petDist} miles</p>
-
-                <div id="addressCont">
-                    <h5>${petLoc.shelter}</h5>
-                    <p>${petLoc.address1}</p>
-                    <p>${petLoc.address2}</p>
-                    <p>${petLoc.city}</p>
-                    <p>${petLoc.state}</p>
-                    <p>${petLoc.zip}</p>
+                <h3 class="card-title">${petBreed}</h3>
+                <div class="main-details">
+                    <span class="price">$${petPrice} | </span>
+                    <span class="age">${petAge}yo | </span>
+                    <span class="distance">${petDist}mi away</span>
                 </div>
-
+                <p>Insert Description</p>
+                </br>
+                <p class='animal-details'>${petLoc.address1}, ${petLoc.address2}</p>
+                <p class='animal-details'>${petLoc.city}, ${petLoc.state} ${petLoc.zip}</p>
             </div>
-        </div>`
+        </div>
+    </div>
+    </div>`
 
     return petCard;
 };
@@ -50,12 +44,34 @@ var petLocation = {
 };
 
 var petName = "Sanders";
-var petImg = "http://placehold.it/200x200";
+var petImg = "https://c.files.bbci.co.uk/12A9B/production/_111434467_gettyimages-1143489763.jpg";
 var petBreed = "Tabby";
 var petAge = "4";
 var petOwners = "1";
 var petDist = "20";
+var petPrice = '500';
 
-var petCard = buildPetCard(petName,petImg,petBreed,petAge,petOwners,petDist,petLocation);
+var petCard = buildPetCard(petName, petImg, petBreed, petAge, petOwners, petDist, petLocation, petPrice);
 petList.appendChild(petCard);
 
+//For testing formatting
+var petCard = buildPetCard(petName, petImg, petBreed, petAge, petOwners, petDist, petLocation, petPrice);
+petList.appendChild(petCard);
+var petCard = buildPetCard(petName, petImg, petBreed, petAge, petOwners, petDist, petLocation, petPrice);
+petList.appendChild(petCard);
+var petCard = buildPetCard(petName, petImg, petBreed, petAge, petOwners, petDist, petLocation, petPrice);
+petList.appendChild(petCard);
+var petCard = buildPetCard(petName, petImg, petBreed, petAge, petOwners, petDist, petLocation, petPrice);
+petList.appendChild(petCard);
+var petCard = buildPetCard(petName, petImg, petBreed, petAge, petOwners, petDist, petLocation, petPrice);
+petList.appendChild(petCard);
+var petCard = buildPetCard(petName, petImg, petBreed, petAge, petOwners, petDist, petLocation, petPrice);
+petList.appendChild(petCard);
+var petCard = buildPetCard(petName, petImg, petBreed, petAge, petOwners, petDist, petLocation, petPrice);
+petList.appendChild(petCard);
+var petCard = buildPetCard(petName, petImg, petBreed, petAge, petOwners, petDist, petLocation, petPrice);
+petList.appendChild(petCard);
+var petCard = buildPetCard(petName, petImg, petBreed, petAge, petOwners, petDist, petLocation, petPrice);
+petList.appendChild(petCard);
+var petCard = buildPetCard(petName, petImg, petBreed, petAge, petOwners, petDist, petLocation, petPrice);
+petList.appendChild(petCard);
