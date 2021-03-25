2cc521dd89cec7f88942ccf7b4fc1ca87f6f9c1f
Old Prego <spymasterluke@gmail.com>
2021-03-24 20:15:57 -0500

    Built function for taking the data from the API fetch results and turning them into cards.

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,30 @@
+// General global variables. petList gets the location of where the HTML templates will be dumped.
+var petCard;
+var petLocation;
+var petList = document.getElementById("petList");
+
+// On loading the results page, this loads the query parameters from the search form
+// It also puts these parameters into variables, so that it is easier to query the API
+var params = new URLSearchParams(document.location.search.substring(1));
+var qCityState = params.get("cityState");
+var qZIP = params.get("zip");
+var qAnimal = params.get("animal");
+var qDistance = params.get("distance");
+var qDogBreed = params.get("dogBreed");
+var qCatBreed = params.get("catBreed");
+var qMinAge = params.get("minAge");
+var qMaxAge = params.get("maxAge");
+var qminPrice = params.get("minPrice");
+var qMaxPrice = params.get("maxPrice");
+
 
 // Template tag parser courtesy of Nate
 // This takes template tags and parses them into HTML elements that are appendable to the current scope
 const html = (strings, ...values) => new DOMParser().parseFromString(strings.map((string, i) => strings[i] + values[i]).join(''), "text/html").body.firstChild;
 
-var petList = document.getElementById("petList");
 
-function buildPetCard(petName,petImg,petBreed,petAge,petOwners,petDist,petLoc){
-    
+// This is the actual template for the HTML, with variables passed to it.
+function buildPetCard(petName,petImg,petBreed,petAge,petDist,petLoc){
     
     let petCard = html`
 
@@ -22,7 +40,6 @@ function buildPetCard(petName,petImg,petBreed,petAge,petOwners,petDist,petLoc){
 
                 <p>Breed: ${petBreed}</p>
                 <p>Age: ${petAge}</p>
-                <p>Previous Owners: ${petOwners}</p>
                 <p>Distance Away: ${petDist} miles</p>
 
                 <div id="addressCont">
@@ -40,45 +57,33 @@ function buildPetCard(petName,petImg,petBreed,petAge,petOwners,petDist,petLoc){
     return petCard;
 };
 
-var petLocation = {
-    shelter: "Luke's Shelter",
-    address1: "315 6th Ave E",
-    address2: "Apt 6",
-    city: "Alexandria",
-    state: "MN",
-    zip: "56308"
+
+// Space for the PetFinder API fetch
+
+
+// This function is not being called yet, but will take data returned from the PetFinder API, build the cards, and append them to the screen.
+function populateCards(data){
+    for (var i = 0; i < data.length; i++){
+
+        petLocation = {
+            email: data[i].contact.email,
+            phone: data[i].contact.phone,
+            address1: data[i].contact.address.address1,
+            address2: data[i].contact.address.address2,
+            city: data[i].contact.address.city,
+            state: data[i].contact.address.state,
+            zip: data[i].contact.address.postcode
+        };
+
+        petCard = buildPetCard(
+            data[i].name,
+            data[i].photos.full,
+            data[i].breed.primary,
+            data[i].age,
+            data[i].distance,
+            petLocation
+            );
+
+        petList.appendChild(petCard);
+    }
 };
-
-var petName = "Sanders";
-var petImg = "http://placehold.it/200x200";
-var petBreed = "Tabby";
-var petAge = "4";
-var petOwners = "1";
-var petDist = "20";
-
-var petCard = buildPetCard(petName,petImg,petBreed,petAge,petOwners,petDist,petLocation);
-petList.appendChild(petCard);
-
-// Search Params kinda look like this:
-http://127.0.0.1:5500/pet-adoption/results.html?cityState=Alexandria%2C+MN&zip=56308&animal=Either&distance=50&dogBreed1=on&dogBreed2=on&dogBreed4=on&catBreed2=on&catBreed3=on&ownerYesNo=on&min=1&max=20&min=800&max=7000
-
-var params = new URLSearchParams(document.location.search.substring(1));
-var qCityState = params.get("cityState");
-var qZIP = params.get("zip");
-var qAnimal = params.get("animal");
-var qDistance = params.get("distance");
-var qDogB1 = params.get("dogBreed1");
-var qDogB2 = params.get("dogBreed2");
-var qDogB3 = params.get("dogBreed3");
-var qDogB4 = params.get("dogBreed4");
-var qCatB1 = params.get("catBreed1");
-var qCatB2 = params.get("catBreed2");
-var qCatB3 = params.get("catBreed3");
-var qCatB4 = params.get("catBreed4");
-var qOwner = params.get("ownerYesNo");
-var qMinAge = params.get("minAge");
-var qMaxAge = params.get("maxAge");
-var qminPrice = params.get("minPrice");
-var qMaxPrice = params.get("maxPrice");
-
-// For toggle switches, TRUE = "on" and FALSE = null. Make sure to convert this for the end parameters.
